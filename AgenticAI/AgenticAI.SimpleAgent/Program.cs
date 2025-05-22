b20d35ec08eb583188c2bf689622aed7e0313425
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-05-22 09:15:03 +0200

    initial checkin with project and description

---
+++ AgenticAI/AgenticAI.SimpleAgent/Program.cs
@@ -0,0 +1,136 @@
+﻿using Azure;
+using Azure.AI.Agents.Persistent;
+using Azure.Identity;
+
+Console.WriteLine("Simple Connected Agent");
+
+#region Env variables
+
+var projectEndpoint = Environment.GetEnvironmentVariable("ProjectEndpoint");
+
+if (string.IsNullOrEmpty(projectEndpoint))
+{
+    Console.WriteLine("Please set the ProjectEndpoint environment variable.");
+    return;
+}
+
+Console.WriteLine("Connecting to the Azure OpenAI service at: " + projectEndpoint);
+
+var modelDeploymentName = Environment.GetEnvironmentVariable("ModelDeploymentName");
+if (string.IsNullOrEmpty(modelDeploymentName))
+{
+    Console.WriteLine("Please set the ModelDeploymentName environment variable.");
+    return;
+}
+
+Console.WriteLine("Using model deployment: " + modelDeploymentName);
+
+var stockAgentId = Environment.GetEnvironmentVariable("StockAgentId");
+if (string.IsNullOrEmpty(stockAgentId))
+{
+    Console.WriteLine("Please set the StockAgentId environment variable.");
+    return;
+}
+
+Console.WriteLine("Using stock agent: " + stockAgentId);
+
+#endregion
+
+#region Agent creation
+
+PersistentAgentsClient client = new(projectEndpoint, new DefaultAzureCredential());
+
+PersistentAgent stockAgent = client.Administration.GetAgent(stockAgentId, CancellationToken.None);
+Console.WriteLine("Connected to stock agent: " + stockAgent.Name);
+
+ConnectedAgentToolDefinition connectedAgentDefinition = new(new ConnectedAgentDetails(stockAgent.Id, stockAgent.Name, "Gets the stock price of a company"));
+
+PersistentAgent mainAgent = client.Administration.CreateAgent(
+    model: modelDeploymentName,
+    name: "stock_price_bot",
+    instructions: "Your job is to get the stock price of a company, using the available tools.",
+    tools: [connectedAgentDefinition]
+);
+
+#endregion
+
+#region Run the agent
+
+PersistentAgentThread thread = client.Threads.CreateThread();
+
+// Create message to thread
+PersistentThreadMessage message = client.Messages.CreateMessage(
+    thread.Id,
+    MessageRole.User,
+    "What is the stock price of Microsoft?");
+
+Console.WriteLine("Sending messages to agents....");
+// Run the agent
+ThreadRun run = client.Runs.CreateRun(thread, mainAgent);
+do
+{
+    Thread.Sleep(TimeSpan.FromMilliseconds(500));
+    run = client.Runs.GetRun(thread.Id, run.Id);
+}
+while (run.Status == RunStatus.Queued
+       || run.Status == RunStatus.InProgress);
+
+Console.WriteLine("Run status: " + run.Status);
+// Confirm that the run completed successfully
+if (run.Status != RunStatus.Completed)
+{
+    throw new Exception("Run did not complete successfully, error: " + run.LastError?.Message);
+}
+
+#endregion
+
+#region List and retrieve messages
+
+Pageable<PersistentThreadMessage> messages = client.Messages.GetMessages(
+    threadId: thread.Id,
+    order: ListSortOrder.Ascending
+);
+
+foreach (var threadMessage in messages)
+{
+    Console.Write($"{threadMessage.CreatedAt:yyyy-MM-dd HH:mm:ss} - {threadMessage.Role,10}: ");
+    foreach (var contentItem in threadMessage.ContentItems)
+    {
+        switch (contentItem)
+        {
+            case MessageTextContent textItem:
+            {
+                var response = textItem.Text;
+                if (textItem.Annotations != null)
+                {
+                    foreach (var annotation in textItem.Annotations)
+                    {
+                        if (annotation is MessageTextUriCitationAnnotation urlAnnotation)
+                        {
+                            response = response.Replace(urlAnnotation.Text, $" [{urlAnnotation.UriCitation.Title}]({urlAnnotation.UriCitation.Uri})");
+                        }
+                    }
+                }
+                Console.Write($"Agent response: {response}");
+                break;
+            }
+            case MessageImageFileContent imageFileItem:
+                Console.Write($"<image from ID: {imageFileItem.FileId}");
+                break;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+#endregion
+
+#region Cleanup
+
+//cleanup
+Console.WriteLine("Cleaning up agents and thread...");
+client.Threads.DeleteThread(thread.Id);
+client.Administration.DeleteAgent(mainAgent.Id);
+client.Administration.DeleteAgent(stockAgent.Id);
+
+#endregion
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# agentic-ai-demo
-demos about agentic AI
+# Demos how to use agentic AI
+
+Repository with demos about Azure Agentic AI approach with demos to show how to use it.
+
+Agents developed using Foundry Agent Service have the following elements:
+
+1. Model: A deployed generative AI model that enables the agent to reason and generate natural language responses to prompts. You can use common OpenAI models and a selection of models from the Azure AI Foundry model catalog.
+2. Knowledge: data sources that enable the agent to ground prompts with contextual data. Potential knowledge sources include Internet search results from Microsoft Bing, an Azure AI Search index, or your own data and documents.
+3. Tools: Programmatic functions that enable the agent to automate actions. Built-in tools to access knowledge in Azure AI Search and Bing are provided as well as a code interpreter tool that you can use to generate and run Python code. You can also create custom tools using your own code or Azure Functions.
+
+![Azure AI foundry](https://learn.microsoft.com/en-us/azure/ai-services/agents/media/agent-service-the-glue.png)
+
+## Demo structure
+
+The demos are structured in the following way:
+
+- **AgenticAI.SimpleAgent**: A simple agent that uses another agent as a tool to get stock price. Code is in `AgenticAI/AgenticAI.SimpleAgent`.
+
+
+To run the demo, you need to set up the environment by installing [.NET](https://dot.net). You can use editors like [Visual Studio](https://visualstudio.microsoft.com/) or [Visual Studio Code](https://code.visualstudio.com/) to help running the demos.
+Dotnet CLI is a cross-platform toolchain for developing, building, running, and publishing .NET applications. You can use it to run the demos from the command line.
+
+Go to the folder where the demo is located and run the following command:
+
+``` powershell
+
+dotnet run
+
+```
+
+# Additional Resources
+
+1. [Azure AI Foundry](https://learn.microsoft.com/en-us/azure/ai-foundry/)
+2. [Azure AI Foundry Agent Service](https://learn.microsoft.com/en-us/azure/ai-foundry/agent-service/)
+3. [Azure AI Foundry Agent Service - Quickstart](https://learn.microsoft.com/en-us/azure/ai-foundry/agent-service/quickstart/)
+4. [AI agent foundamentals](https://learn.microsoft.com/en-us/training/modules/ai-agent-fundamentals)
